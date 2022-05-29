0c403df924798e869e8373408536f984f455bb79
xqm32 <458173774@qq.com>
2022-05-29 23:32:22 +0800

    init commit

---
+++ HRMS/admin.py
@@ -0,0 +1,26 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+
+from HRMS.auth import login_required
+from HRMS.db import get_db
+
+bp = Blueprint("admin", __name__)
+
+
+@bp.route("/")
+@login_required
+def index():
+    """Show all the posts, most recent first."""
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("admin/index.html", posts=posts)
