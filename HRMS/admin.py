b5c50c96d85d543631a2e1003f07d82ad182f6ba
xqm32 <458173774@qq.com>
2022-06-01 13:39:25 +0800

    add `CRUD`

--- HRMS/admin.py
+++ HRMS/admin.py
@@ -20,14 +20,73 @@ from HRMS.db import get_db
 bp = Blueprint("admin", __name__)
 
 
-def get_columns(table: str):
+def select(table, column_name, when=None):
+    if when:
+        return [i[column_name] for i in table if when(i)]
+    else:
+        return [i[column_name] for i in table]
+
+
+def to_where_clause(args):
+    return " AND".join(f" {i}={args[i]}" for i in args)
+
+
+def to_args(columns, row):
+    return "&".join(f"{j}={str(row[j])}" for j in columns)
+
+
+def get_columns(table: str, with_pk=False, with_fk=False):
     db = get_db()
+    ret = dict()
+
     table_schema = db.execute(f"PRAGMA table_info({table})").fetchall()
-    table_columns = [row["name"] for row in table_schema]
-    return table_columns
+
+    table_columns = select(table_schema, "name")
+    ret.update({"columns": table_columns})
+
+    if with_pk:
+        table_pk = select(table_schema, "name", lambda i: i["pk"])
+        ret.update({"pk": table_pk})
+
+    if with_fk:
+        table_fk = db.execute(f"PRAGMA foreign_key_list({table})").fetchall()
+        ret.update({"fk": table_fk})
+
+    return ret
 
 
-# TODO immutable_columns 从 config.py 中读取，如 immutable_columns[table]
+def create_table(
+    table: str,
+    immutable_columns: Iterable[str],
+    form: Dict,
+    message=(),
+):
+    db = get_db()
+    table_columns = get_columns(table)["columns"]
+
+    for i in immutable_columns:
+        if i in table_columns:
+            table_columns.remove(i)
+
+    columns = ", ".join(f"{i}" for i in table_columns)
+    values = ", ".join(f"?" for i in table_columns)
+
+    print(f"INSERT INTO {table}({columns}) VALUES ({values})")
+    print(f"{list(form[i] for i in table_columns)}")
+
+    try:
+        db.execute(
+            f"INSERT INTO {table}({columns}) VALUES ({values})",
+            [form[i] for i in table_columns],
+        )
+        db.commit()
+    except Exception as e:
+        flash(e, "error")
+    else:
+        if message:
+            flash(*message)
+
+
 def update_table(
     table: str,
     immutable_columns: Iterable[str],
@@ -36,7 +95,7 @@ def update_table(
     message=(),
 ):
     db = get_db()
-    table_columns = get_columns(table)
+    table_columns = get_columns(table)["columns"]
 
     for i in immutable_columns:
         if i in table_columns:
@@ -72,15 +131,17 @@ def index():
         "SELECT COUNT(*) AS COUNT FROM 考勤信息表 WHERE 考勤日期 = ?",
         (maya.now().datetime(to_timezone="Asia/Shanghai").strftime("%Y-%m-%d"),),
     ).fetchone()
+
     try:
         attendance_rate = f"{attendance_count['COUNT']/employee_count['COUNT']*100:.2f}"
     except ZeroDivisionError:
         attendance_rate = "0.00"
-    
+
     wages_sum = db.execute(
         "SELECT SUM(应发工资) AS SUM FROM 工资计发信息表 WHERE 计发日期 = ?",
         (maya.now().datetime(to_timezone="Asia/Shanghai").strftime("%Y-%m"),),
     ).fetchone()
+
     if wages_sum["SUM"] is None:
         total_wages = "0.00"
     else:
@@ -96,9 +157,9 @@ def index():
     return render_template("admin/index.html", table=table)
 
 
-@bp.route("/user/modify", methods=["GET", "POST"])
+@bp.route("/user/update", methods=["GET", "POST"])
 @login_required
-def modify():
+def user_update():
     db = get_db()
 
     immutable_columns = {"用户编号", "用户类型", "权限", "用户名"}
@@ -117,6 +178,7 @@ def modify():
             g.user_info = db.execute(
                 "SELECT * FROM 用户信息表 WHERE 用户编号 = ?", (g.user["用户编号"],)
             ).fetchone()
+
         elif request.form["table"] == "user":
             form = dict(request.form)
             if form["密码"] == form["确认密码"]:
@@ -143,8 +205,107 @@ def modify():
     table = {"user": user, "user_info": user_info}
 
     return render_template(
-        "admin/user/modify.html",
+        "admin/user/update.html",
         table=table,
         input_types=input_types,
         immutable_columns=immutable_columns,
     )
+
+
+@bp.route("/create/<table_name>", methods=["GET", "POST"])
+@login_required
+def create(table_name):
+    ret = get_columns(table_name, with_pk=True, with_fk=True)
+    table_columns = ret["columns"]
+    table_pk = ret["pk"]
+    table_fk = select(ret["fk"], "from")
+    immutable_columns = []
+
+    # 去除自动编号
+    for i in table_pk:
+        if "编号" in i and i not in table_fk:
+            immutable_columns.append(i)
+
+    if request.method == "POST":
+        create_table(table_name, immutable_columns, request.form, ("添加成功", "success"))
+        return redirect(url_for("admin.retrieve", table_name=table_name))
+
+    return render_template(
+        "admin/create-base.html",
+        table_name=table_name,
+        columns=table_columns,
+        immutable_columns=immutable_columns,
+    )
+
+
+@bp.route("/retrieve/<table_name>", methods=["GET"])
+@login_required
+def retrieve(table_name):
+    db = get_db()
+
+    content = db.execute(f"SELECT * FROM {table_name}").fetchall()
+    ret = get_columns(table_name, with_pk=True)
+    table_columns = ret["columns"]
+    table_pk = ret["pk"]
+
+    header = table_columns
+    content = [
+        {"row": i, "pk": "&".join(f"{j}={str(i[j])}" for j in table_pk)}
+        for i in content
+    ]
+
+    return render_template(
+        "admin/retrieve-base.html",
+        title=table_name,
+        table_name=table_name,
+        header=header,
+        content=content,
+    )
+
+
+@bp.route("/update/<table_name>", methods=["GET", "POST"])
+@login_required
+def update(table_name):
+    db = get_db()
+    ret = get_columns(table_name, with_pk=True)
+    table_pk = ret["pk"]
+
+    if request.method == "POST":
+        update_table(
+            table_name,
+            table_pk,
+            request.form,
+            to_where_clause(request.args),
+            ("修改成功", "success"),
+        )
+        return redirect(url_for("admin.retrieve", table_name=table_name))
+
+    row = db.execute(
+        f"SELECT * FROM {table_name} WHERE {to_where_clause(request.args)}"
+    ).fetchone()
+
+    if row is None:
+        abort(404)
+
+    return render_template(
+        "admin/update-base.html",
+        table_name=table_name,
+        row=row,
+        immutable_columns=table_pk,
+    )
+
+
+@bp.route("/delete/<table_name>", methods=["GET"])
+@login_required
+def delete(table_name):
+    db = get_db()
+
+    try:
+        db.execute(f"DELETE FROM {table_name} WHERE {to_where_clause(request.args)}")
+        db.commit()
+    except Exception as e:
+        flash(e, "error")
+    else:
+        flash("删除成功", "success")
+
+    return redirect(url_for("admin.retrieve", table_name=table_name))
--- HRMS/schema.sql
+++ HRMS/schema.sql
@@ -1,42 +1,194 @@
-
-DROP TABLE IF EXISTS 原单位信息表;
-CREATE TABLE 原单位信息表 (员工编号 INTEGER NOT NULL, 原单位 TEXT, 原职称 TEXT, 原职务 TEXT, PRIMARY KEY (员工编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 员工基本信息表;
-CREATE TABLE 员工基本信息表 (员工编号 INTEGER NOT NULL, 姓名 TEXT NOT NULL, 性别 TEXT NOT NULL, 所属部门编号 TEXT, 进入部门日期 TEXT NOT NULL, 职务代码 TEXT NOT NULL, 职称 TEXT, 起薪日 TEXT, 在岗状态 TEXT NOT NULL, PRIMARY KEY (员工编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 员工学历信息表;
-CREATE TABLE 员工学历信息表 (员工编号 INTEGER NOT NULL, 毕业学校 TEXT, 毕业日期 TEXT, 学历 TEXT, 专业 TEXT, 外语 TEXT, PRIMARY KEY (员工编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 员工录用信息表;
-CREATE TABLE 员工录用信息表 (签约合同号 INTEGER NOT NULL, 员工编号 TEXT NOT NULL, 姓名 TEXT NOT NULL, 签约日期 TEXT NOT NULL, 到期日 TEXT NOT NULL, 合同类型 TEXT NOT NULL, 受聘部门 TEXT NOT NULL, 受聘职务 TEXT NOT NULL, 聘用标志 TEXT NOT NULL, 备注 TEXT, PRIMARY KEY (签约合同号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 员工私人信息表;
-CREATE TABLE 员工私人信息表 (员工编号 INTEGER NOT NULL, 姓名 TEXT NOT NULL, 地址 TEXT, 电话 TEXT, 邮箱 TEXT, 出生年月 TEXT, 籍贯 TEXT, 民族 TEXT, 身份证号 TEXT NOT NULL, 政治面貌 TEXT, PRIMARY KEY (员工编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 工作考核信息表;
-CREATE TABLE 工作考核信息表 (员工编号 INTEGER NOT NULL, 考核日期 TEXT NOT NULL, 工作态度 TEXT, 工作业绩 TEXT, 业务水平 TEXT, 考核结论 TEXT, 备注 TEXT, PRIMARY KEY (员工编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 工资标准信息表;
-CREATE TABLE 工资标准信息表 (工资等级编号 INTEGER NOT NULL, 等级名称 TEXT NOT NULL, 底薪 REAL NOT NULL, 补贴 REAL, 奖金 REAL, 车补 REAL, 房补 REAL, 养老保险 REAL, 医疗保险 REAL, 住房公积金 REAL, PRIMARY KEY (工资等级编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 工资计发信息表;
-CREATE TABLE 工资计发信息表 (自动编号 INTEGER NOT NULL, 员工编号 TEXT NOT NULL, 工资等级编号 TEXT NOT NULL, 底薪 REAL NOT NULL, 补贴 REAL, 奖金 REAL, 车补 REAL, 房补 REAL, 扣考核 REAL, 加班费 REAL, 代扣养老保险 REAL, 代扣医疗保险 REAL, 代扣住房公积金 REAL, 税前小计 REAL, 税率 REAL, 应发工资 REAL, 计发日期 TEXT NOT NULL, PRIMARY KEY (自动编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 用户信息表;
-CREATE TABLE 用户信息表 (用户编号 TEXT NOT NULL, 真实姓名 TEXT NOT NULL, 用户类型 TEXT DEFAULT 普通用户, 权限 TEXT DEFAULT 10, 身份证号 TEXT, 住址 TEXT, 电话 TEXT, 邮箱 TEXT);
-
-DROP TABLE IF EXISTS 用户验证表;
-CREATE TABLE 用户验证表 (用户编号 INTEGER NOT NULL, 用户名 TEXT UNIQUE NOT NULL, 密码 TEXT, PRIMARY KEY (用户编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 考勤信息表;
-CREATE TABLE 考勤信息表 (自动编号 INTEGER NOT NULL, 员工编号 TEXT NOT NULL, 考勤日期 TEXT NOT NULL, 考勤类型 TEXT NOT NULL, 考勤天数 TEXT NOT NULL, 备注 TEXT NOT NULL, PRIMARY KEY (自动编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 考勤考核信息表;
-CREATE TABLE 考勤考核信息表 (自动编号 INTEGER NOT NULL, 员工编号 TEXT NOT NULL, 出勤日期 TEXT NOT NULL, 奖励 TEXT NOT NULL, 惩罚 TEXT NOT NULL, 加班费 REAL NOT NULL, 扣考核 REAL, PRIMARY KEY (自动编号 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 职务信息表;
-CREATE TABLE 职务信息表 (职务代码 INTEGER NOT NULL, 职务名称 TEXT NOT NULL, 工资等级 TEXT NOT NULL, 工资上限 REAL NOT NULL, 工资下限 REAL NOT NULL, 简介 TEXT, PRIMARY KEY (职务代码 AUTOINCREMENT));
-
-DROP TABLE IF EXISTS 部门信息表;
-CREATE TABLE 部门信息表 (部门代码 INTEGER NOT NULL, 部门名称 TEXT NOT NULL, 领导姓名 TEXT NOT NULL, 住址 TEXT, 电话 TEXT, 简介 TEXT NOT NULL, PRIMARY KEY (部门代码 AUTOINCREMENT));
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "原单位信息表";
+CREATE TABLE IF NOT EXISTS "原单位信息表" (
+	"员工编号"	INTEGER NOT NULL,
+	"原单位"	TEXT,
+	"原职称"	TEXT,
+	"原职务"	TEXT,
+	PRIMARY KEY("员工编号" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "员工基本信息表";
+CREATE TABLE IF NOT EXISTS "员工基本信息表" (
+	"员工编号"	INTEGER NOT NULL,
+	"姓名"	TEXT NOT NULL,
+	"性别"	TEXT NOT NULL,
+	"所属部门编号"	TEXT,
+	"进入部门日期"	TEXT NOT NULL,
+	"职务代码"	TEXT NOT NULL,
+	"职称"	TEXT,
+	"起薪日"	TEXT,
+	"在岗状态"	TEXT NOT NULL,
+	PRIMARY KEY("员工编号" AUTOINCREMENT),
+	FOREIGN KEY("职务代码") REFERENCES "职务信息表"("职务代码")
+);
+DROP TABLE IF EXISTS "员工学历信息表";
+CREATE TABLE IF NOT EXISTS "员工学历信息表" (
+	"员工编号"	INTEGER NOT NULL,
+	"毕业学校"	TEXT,
+	"毕业日期"	TEXT,
+	"学历"	TEXT,
+	"专业"	TEXT,
+	"外语"	TEXT,
+	PRIMARY KEY("员工编号" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "员工录用信息表";
+CREATE TABLE IF NOT EXISTS "员工录用信息表" (
+	"签约合同号"	TEXT NOT NULL,
+	"员工编号"	TEXT NOT NULL,
+	"姓名"	TEXT NOT NULL,
+	"签约日期"	TEXT NOT NULL,
+	"到期日"	TEXT NOT NULL,
+	"合同类型"	TEXT NOT NULL,
+	"受聘部门"	TEXT NOT NULL,
+	"受聘职务"	TEXT NOT NULL,
+	"聘用标志"	TEXT NOT NULL,
+	"备注"	TEXT,
+	PRIMARY KEY("签约合同号"),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号")
+);
+DROP TABLE IF EXISTS "员工私人信息表";
+CREATE TABLE IF NOT EXISTS "员工私人信息表" (
+	"员工编号"	TEXT NOT NULL,
+	"姓名"	TEXT NOT NULL,
+	"地址"	TEXT,
+	"电话"	TEXT,
+	"邮箱"	TEXT,
+	"出生年月"	TEXT,
+	"籍贯"	TEXT,
+	"民族"	TEXT,
+	"身份证号"	TEXT NOT NULL,
+	"政治面貌"	TEXT,
+	PRIMARY KEY("员工编号"),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号")
+);
+DROP TABLE IF EXISTS "家庭关系表";
+CREATE TABLE IF NOT EXISTS "家庭关系表" (
+	"员工编号"	TEXT NOT NULL,
+	"家庭编号"	TEXT NOT NULL,
+	"姓名"	VARCHAR NOT NULL,
+	"性别"	TEXT,
+	"年龄"	INTEGER,
+	"家属关系"	VARCHAR,
+	"工作单位"	VARCHAR,
+	PRIMARY KEY("员工编号"),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号")
+);
+DROP TABLE IF EXISTS "工作考核信息表";
+CREATE TABLE IF NOT EXISTS "工作考核信息表" (
+	"员工编号"	TEXT NOT NULL,
+	"考核日期"	TEXT NOT NULL,
+	"工作态度"	TEXT,
+	"工作业绩"	TEXT,
+	"业务水平"	TEXT,
+	"考核结论"	TEXT,
+	"备注"	TEXT,
+	PRIMARY KEY("员工编号"),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号")
+);
+DROP TABLE IF EXISTS "工资标准信息表";
+CREATE TABLE IF NOT EXISTS "工资标准信息表" (
+	"工资等级编号"	INTEGER NOT NULL,
+	"等级名称"	TEXT NOT NULL,
+	"底薪"	REAL NOT NULL,
+	"补贴"	REAL,
+	"奖金"	REAL,
+	"车补"	REAL,
+	"房补"	REAL,
+	"养老保险"	REAL,
+	"医疗保险"	REAL,
+	"住房公积金"	REAL,
+	PRIMARY KEY("工资等级编号" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "工资计发信息表";
+CREATE TABLE IF NOT EXISTS "工资计发信息表" (
+	"自动编号"	INTEGER NOT NULL,
+	"员工编号"	TEXT NOT NULL,
+	"工资等级编号"	TEXT NOT NULL,
+	"底薪"	REAL NOT NULL,
+	"补贴"	REAL,
+	"奖金"	REAL,
+	"车补"	REAL,
+	"房补"	REAL,
+	"扣考核"	REAL,
+	"加班费"	REAL,
+	"代扣养老保险"	REAL,
+	"代扣医疗保险"	REAL,
+	"代扣住房公积金"	REAL,
+	"税前小计"	REAL,
+	"税率"	REAL,
+	"应发工资"	REAL,
+	"计发日期"	TEXT NOT NULL,
+	PRIMARY KEY("自动编号" AUTOINCREMENT),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号"),
+	FOREIGN KEY("工资等级编号") REFERENCES "工资标准信息表"("工资等级编号")
+);
+DROP TABLE IF EXISTS "用户信息表";
+CREATE TABLE IF NOT EXISTS "用户信息表" (
+	"用户编号"	TEXT NOT NULL,
+	"真实姓名"	TEXT NOT NULL,
+	"用户类型"	TEXT DEFAULT 普通用户,
+	"权限"	TEXT DEFAULT 10,
+	"身份证号"	TEXT,
+	"住址"	TEXT,
+	"电话"	TEXT,
+	"邮箱"	TEXT,
+	PRIMARY KEY("用户编号"),
+	FOREIGN KEY("用户编号") REFERENCES "用户验证表"("用户编号")
+);
+DROP TABLE IF EXISTS "用户验证表";
+CREATE TABLE IF NOT EXISTS "用户验证表" (
+	"用户编号"	INTEGER NOT NULL,
+	"用户名"	TEXT NOT NULL UNIQUE,
+	"密码"	TEXT,
+	PRIMARY KEY("用户编号" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "考勤信息表";
+CREATE TABLE IF NOT EXISTS "考勤信息表" (
+	"自动编号"	INTEGER NOT NULL,
+	"员工编号"	TEXT NOT NULL,
+	"考勤日期"	TEXT NOT NULL,
+	"考勤类型"	TEXT NOT NULL,
+	"考勤天数"	TEXT NOT NULL,
+	"备注"	TEXT NOT NULL,
+	PRIMARY KEY("自动编号" AUTOINCREMENT),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号")
+);
+DROP TABLE IF EXISTS "考勤考核信息表";
+CREATE TABLE IF NOT EXISTS "考勤考核信息表" (
+	"自动编号"	INTEGER NOT NULL,
+	"员工编号"	TEXT NOT NULL,
+	"出勤日期"	TEXT NOT NULL,
+	"奖励"	TEXT NOT NULL,
+	"惩罚"	TEXT NOT NULL,
+	"加班费"	REAL NOT NULL,
+	"扣考核"	REAL,
+	PRIMARY KEY("自动编号" AUTOINCREMENT),
+	FOREIGN KEY("员工编号") REFERENCES "员工基本信息表"("员工编号")
+);
+DROP TABLE IF EXISTS "职务信息表";
+CREATE TABLE IF NOT EXISTS "职务信息表" (
+	"职务代码"	TEXT NOT NULL,
+	"职务名称"	TEXT NOT NULL,
+	"工资等级"	TEXT NOT NULL,
+	"工资上限"	REAL NOT NULL,
+	"工资下限"	REAL NOT NULL,
+	"简介"	TEXT,
+	PRIMARY KEY("职务代码")
+);
+DROP TABLE IF EXISTS "部门信息表";
+CREATE TABLE IF NOT EXISTS "部门信息表" (
+	"部门代码"	TEXT NOT NULL,
+	"部门名称"	TEXT NOT NULL,
+	"领导姓名"	TEXT NOT NULL,
+	"住址"	TEXT,
+	"电话"	TEXT,
+	"简介"	TEXT NOT NULL,
+	PRIMARY KEY("部门代码")
+);
+DROP TRIGGER IF EXISTS "删除用户触发器";
+CREATE TRIGGER 删除用户触发器 AFTER DELETE 
+ON 用户信息表
+BEGIN
+    DELETE FROM 用户验证表 WHERE 用户编号 = old.用户编号;
+END;
+COMMIT;
